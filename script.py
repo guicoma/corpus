a71edc5790602717372868ff66c0374047267e27
Coma <guillemcoma@gmail.com>
2019-05-25 17:56:05 +0200

    refactor script with beautiful soup

--- script.py
+++ script.py
@@ -1,27 +1,10 @@
 import os
 import json
+import re
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 from recursivejson import extract_values
 from xml.dom.minidom import parse, parseString
-
-
-def iterator(parents, nested=False):
-    for child in reversed(parents):
-        if nested:
-            if len(child) >= 1:
-                iterator(child, nested=True)
-        if child.tag == 'textarea':
-            parents.remove(child)
-        elif child.tag == 'i':
-            parents.remove(child)
-        elif child.tag == 'em':
-            parents.remove(child)
-        elif child.tag == 'st1':
-            parents.remove(child)
-        elif 'class' in child.attrib:
-            if 'Emphasis' in child.get('class'):
-                parents.remove(child)
 
 files = os.listdir('source')
 
@@ -50,46 +33,89 @@ for file in files:
 
     for idx, page in enumerate(responseBody):
         soup = BeautifulSoup(page, 'html.parser')
-        all_inputs = soup.find_all('input')
         
-        for t_input in all_inputs:
-            t_input.decompose()
+        all_comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        [comment.extract() for comment in all_comments]
+        
+        all_br = soup.find_all('br')
+        all_em = soup.find_all('em')
+        all_emphasis = soup.find_all(class_="Emphasis")
+        all_personajes = soup.find_all(class_="personaje_teatro")
+        all_heads = soup.find_all('head')
+        all_i = soup.find_all('i')
+        all_inputs = soup.find_all('input')
+        all_ops = soup.find_all('o:p')
+        all_stds = soup.find_all(re.compile("st1"))
+        all_styles = soup.find_all('style')
+        all_txtareas = soup.find_all('textarea')
 
-        pageSanitized = unicode(soup)
+        all_tags = all_br + all_em + all_emphasis + all_i + all_inputs + all_heads + all_ops + all_personajes + all_stds + all_styles + all_txtareas
 
-        string = '<source>'+pageSanitized+'</source>'
-        string = string.replace('w:st', 'st').replace(':city', '').replace(':place', '').replace(':street', '').replace(':address', '').replace('\r\n', '').replace('[', '').replace(']', '').replace('<p', '\r\n<p').replace('<br>', '\r\n').strip().encode('utf-8')
+        for tag in all_tags:
+            tag.decompose()
 
-        try:
-            root = ET.fromstring(string)
-            if len(list(root)) <= 0:
-                raise Exception('Tag seems not to contain the text, perhaps it is an image:')
-            iterator(root, nested=True)
-            resultText = ET.tostring(root, encoding='utf-8', method='text')
-            print "----",idx,"----"
+        for string in soup.stripped_strings:
+            decodeStr = string.replace('[', '').replace(']', '')+'\n'
+            resultLine = decodeStr.encode('utf-8')
             if idx % 2 == 0:
-                f2.write(resultText)
-                # print "Spanish"
-                # print resultText[0:20]
+                f2.write(resultLine)
             else:
-                f1.write(resultText)
-                # print "English"
-                # print resultText[0:20]
-            
-        except ET.ParseError as p_error:
-            print 'Parse error',string
-            print p_error
-            continue
-        except Exception as g_error:
-            print 'General error'
-            print g_error
-            continue
-        except:
-            print 'Oops'
-            continue
-
+                f1.write(resultLine)
 
     f1.close()
     f2.close()
     read_file.close()
+        #raise Exception("Debug")
+
+        # def iterator(parents, nested=False):
+        #     for child in reversed(parents):
+        #         if nested:
+        #             if len(child) >= 1:
+        #                 iterator(child, nested=True)
+        #         if child.tag == 'textarea':
+        #             parents.remove(child)
+        #         elif child.tag == 'i':
+        #             parents.remove(child)
+        #         elif child.tag == 'em':
+        #             parents.remove(child)
+        #         elif 'class' in child.attrib:
+        #             if 'Emphasis' in child.get('class'):
+        #                 parents.remove(child)
+
+        # string = unicode(soup)
+        # stringSource = '<source>'+string+'</source>'
+        # pageSanitized = stringSource.replace('\r\n', '').replace('[', '').replace(']', '').replace('<p', '\r\n<p').replace('<br>', '\r\n').replace('<br/>', '\r\n').strip().encode('utf-8')
+
+        # try:
+        #     root = ET.fromstring(pageSanitized)
+        #     if len(list(root)) <= 0:
+        #         raise Exception('Tag seems not to contain the text, perhaps it is an image:')
+        #     #iterator(root, nested=True)
+        #     resultText = ET.tostring(root, encoding='utf-8', method='text')
+        #     print "----",idx,"----"
+        #     if idx % 2 == 0:
+        #         f2.write(resultText)
+        #         # print "Spanish"
+        #         # print resultText[0:20]
+        #     else:
+        #         f1.write(resultText)
+        #         # print "English"
+        #         # print resultText[0:20]
+            
+        # except ET.ParseError as p_error:
+        #     print 'Parse error',pageSanitized
+        #     print p_error
+        #     continue
+        # except Exception as g_error:
+        #     print 'General error'
+        #     print g_error
+        #     continue
+        # except:
+        #     print 'Oops'
+        #     continue
+
+
+    # f1.close()
+    # f2.close()
+    # read_file.close()
     #print(resultText)
